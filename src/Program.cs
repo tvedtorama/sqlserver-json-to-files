d207a3ec5cf73234d2a476dc05c237f943711480
Jørgen Tvedt <tvedtorama@gmail.com>
2018-05-30 10:19:23 +0200

    Basics for console app.

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sqlserver_json_to_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
