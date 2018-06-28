b34321355862ebfb62ee5f65cbcf57668846c75a
Kjell-Morten <post@kjellmorten.no>
2018-06-28 17:17:19 +0200

    Shorten ISO and EM keys

--- src/FetchIdentities.sql
+++ src/FetchIdentities.sql
@@ -1,10 +1,8 @@
 	SELECT Chip.Serienummer as identityId,
 	    -- CAST(Chip.IDBrikke as nvarchar(200)) AS identityId,
 		ChipCategory.KortNavn as identityType,
-		Chip.RUIDEM4102 as "externalKeys.RUIDEM4102",
-		Chip.RUIDISO14443A as "externalKeys.RUIDISO14443A",
-		Chip.UIDEM4102 as "externalKeys.UIDEM4102",
-		Chip.UIDISO14443A as "externalKeys.UIDISO14443A",
+		Chip.UIDEM4102 as "externalKeys.EM",
+		Chip.UIDISO14443A as "externalKeys.ISO",
 		CAST(Chip.IDBrikke as nvarchar(200)) AS "externalKeys.BossID",
 		CASE CC.Sperret WHEN 1 THEN 'DISABLED' ELSE NULL END as 'status'
 		FROM Brikker Chip INNER JOIN BrikkeKategori ChipCategory ON Chip.IDBrikkeKategori = ChipCategory.IDBrikkeKategori
