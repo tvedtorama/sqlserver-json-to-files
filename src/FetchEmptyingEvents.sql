27e440aeb6bd6c3d1239065eaaa6c59c94dabd85
Jørgen Tvedt <tvedtorama@gmail.com>
2018-09-21 08:05:52 +0200

    Fixed ambigious colum error

--- src/FetchEmptyingEvents.sql
+++ src/FetchEmptyingEvents.sql
@@ -11,7 +11,7 @@ SELECT
 				'ID' as pointRefType,
 				CASE WHEN K.IDTommeHendelseType = 1 THEN 'S' + CONVERT(nvarchar, TE.IDTjeneste) ELSE NULL END as identityId,
 				CASE WHEN K.IDTommeHendelseType = 1 THEN 'POINT_REF' ELSE NULL END as identityType,
-				CASE IDFraksjon WHEN 1 THEN '0001' WHEN 2 THEN '9999' WHEN 3 THEN '1299' WHEN 6 THEN '1231' WHEN 7 THEN '1700' WHEN 8 THEN '1261' END as "properties.wasteCategory"
+				CASE K.IDFraksjon WHEN 1 THEN '0001' WHEN 2 THEN '9999' WHEN 3 THEN '1299' WHEN 6 THEN '1231' WHEN 7 THEN '1700' WHEN 8 THEN '1261' END as "properties.wasteCategory"
 				FROM TommeHendelser K 
 					LEFT OUTER JOIN TommeEnhet TE ON K.IDTommeEnhet = TE.IDTommeEnhet
 				-- Note: When the IDTjeneste condition was added, to fix duplicate events among services, the query slowed down considerably
