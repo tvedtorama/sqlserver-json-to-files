19c9c60d8e46bf65f0dcfa67f9e7f015724e1d31
WasteIQ BIR <tvedtorama@gmail.com>
2019-01-28 09:20:08 +0100

    Challenging rewrite of points query to avoid weird union-crash related to json block.

--- src/FetchPoints.sql
+++ src/FetchPoints.sql
@@ -1,11 +1,11 @@
 /* Union terminal, points and point children into point hierarchy */
-WITH AccessPoint (Id, bossIdId, Guid, IDPunktType, IDFraksjon, IDPunktEgenskap, IDPunktKundeTyper, ServiceGuid, ServiceId, ServiceRef, ParentId, tag, name, description, geoLocation, status, oldCustomerFn) 
+WITH AccessPoint (Id, bossIdId, IsGeoLocation, Guid, IDPunktType, IDFraksjon, IDPunktEgenskap, IDPunktKundeTyper, ServiceGuid, ServiceId, ServiceRef, ParentId, tag, name, description, status, oldCustomerFn) 
 	  AS (
-	     SELECT CAST(IDPunkt as nvarchar(200)) as ID, IDPunkt as bossIdId, GUIDPunkt as Guid, IDPunktType, IDFraksjon, IDPunktEgenskap, IDPunktKundeTyper, P.TjenesteGuidPunkt as ServiceGuid, P.TjenesteIdPunkt as ServiceId, P.TjenesteReferanse as ServiceRef, 'S' + CAST(IDTjeneste as nvarchar(200)) as ParentId, CASE WHEN P.Merkelapp = 'NA' THEN NULL ELSE P.Merkelapp END as tag, Navn as name, Beskrivelse as description, (SELECT p2.UtmSone as utmZone, p2.UtmX as utmX, p2.UtmY as utmY, p2.GPS as gps, p2.DesimalGrader as decimalDegrees FROM [BossID].[dbo].Punkt P2 WHERE P2.IDPunkt = P.IDPunkt FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER) AS geoLocation, 'Active' AS status, null AS oldCustomerFn FROM [BossID].[dbo].Punkt P WHERE Slettet <> 1
+	     SELECT CAST(IDPunkt as nvarchar(200)) as ID, IDPunkt as bossIdId, CASE WHEN DesimalGrader IS NOT NULL OR UtmX IS NOT NULL THEN 1 ELSE 0 END AS IsGeoLocation, GUIDPunkt as Guid, IDPunktType, IDFraksjon, IDPunktEgenskap, IDPunktKundeTyper, P.TjenesteGuidPunkt as ServiceGuid, P.TjenesteIdPunkt as ServiceId, P.TjenesteReferanse as ServiceRef, 'S' + CAST(IDTjeneste as nvarchar(200)) as ParentId, CASE WHEN P.Merkelapp = 'NA' THEN NULL ELSE P.Merkelapp END as tag, Navn as name, Beskrivelse as description, 'Active' AS status, null AS oldCustomerFn FROM [BossID].[dbo].Punkt P WHERE Slettet <> 1
 		 UNION
-		 SELECT 'C' + CAST(IDPunktBarn as nvarchar(200)) AS ID, IDPunktBarn as bossIdId, NULL as Guid, IDPunktType, IDFraksjon, IDPunktEgenskap, IDPunktKundeTyper, PB.TjenesteGuidPunkt as ServiceGuid, PB.TjenesteIdPunkt as ServiceId, PB.TjenesteReferanse AS ServiceRef, CASE WHEN PB.IDPunktFar IS NULL THEN CAST(PB.IDPunkt as nvarchar(200)) ELSE 'C' + CAST(IDPunktFar as nvarchar(200)) END, NULL as tag, NULL as Name, null as description, NULL as geoLocation, 'Active' AS status, null AS oldCustomerFn FROM [BossID].[dbo].PunktBarn PB WHERE Slettet <> 1
+		 SELECT 'C' + CAST(IDPunktBarn as nvarchar(200)) AS ID, IDPunktBarn as bossIdId, 0 as IsGeoLocation, NULL as Guid, IDPunktType, IDFraksjon, IDPunktEgenskap, IDPunktKundeTyper, PB.TjenesteGuidPunkt as ServiceGuid, PB.TjenesteIdPunkt as ServiceId, PB.TjenesteReferanse AS ServiceRef, CASE WHEN PB.IDPunktFar IS NULL THEN CAST(PB.IDPunkt as nvarchar(200)) ELSE 'C' + CAST(IDPunktFar as nvarchar(200)) END, NULL as tag, NULL as Name, null as description, 'Active' AS status, null AS oldCustomerFn FROM [BossID].[dbo].PunktBarn PB WHERE Slettet <> 1
 		 UNION
-		 SELECT 'S' + CAST(IDTjeneste as nvarchar(200)) as ID, IDTjeneste as bossIdId, GUIDTjeneste as Guid, 10 as IDPunktType, 1 as IDFraksjon, 0 as IDPunktEgenskap, 0 as IDPunktKundeTyper, null as ServiceGuid, null as ServiceId, '' as ServiceRef, '' as ParentID, NULL as tag, Navn as Name, Beskrivelse as description, NULL as geoLocation, CASE WHEN ServiceStatus = 1 THEN 'Active' ELSE 'Inactive' END AS status, CASE WHEN I3&128 = 0 THEN 1 ELSE 0 END AS oldCustomerFn FROM [BossID].[dbo].BossIDTjeneste WHERE Slettet <> 1
+		 SELECT 'S' + CAST(IDTjeneste as nvarchar(200)) as ID, IDTjeneste as bossIdId, 0 as IsGeoLocation, GUIDTjeneste as Guid, 10 as IDPunktType, 1 as IDFraksjon, 0 as IDPunktEgenskap, 0 as IDPunktKundeTyper, null as ServiceGuid, null as ServiceId, '' as ServiceRef, '' as ParentID, NULL as tag, Navn as Name, Beskrivelse as description, CASE WHEN ServiceStatus = 1 THEN 'Active' ELSE 'Inactive' END AS status, CASE WHEN I3&128 = 0 THEN 1 ELSE 0 END AS oldCustomerFn FROM [BossID].[dbo].BossIDTjeneste WHERE Slettet <> 1
 	  )
 	  SELECT id as accessPointId, parentId as parentId, name, description,
 	  CASE WHEN PT.KortNavn = 'NEDPUNKT' THEN 'ACCESS_PARENT' WHEN PT.KortNavn = 'NEDKAST' THEN 'ACCESS_POINT' WHEN PT.KortNavn = 'GRUPPE' THEN 'GROUP' ELSE 'TERMINAL' END as type,
@@ -24,7 +24,7 @@ WITH AccessPoint (Id, bossIdId, Guid, IDPunktType, IDFraksjon, IDPunktEgenskap, 
 	  BS.ServiceBinding AS 'properties.serviceBinding',
 	  BT.navn AS 'properties.RFIDReadFormat', 
 	  oldCustomerFn AS 'properties.oldCustomerFn',
-	  JSON_QUERY(geoLocation) AS geoLocation,
+	  JSON_QUERY(CASE WHEN IsGeoLocation = 1 THEN (SELECT p2.UtmSone as utmZone, p2.UtmX as utmX, p2.UtmY as utmY, p2.GPS as gps, p2.DesimalGrader as decimalDegrees FROM [BossID].[dbo].Punkt P2 WHERE P2.IDPunkt = bossIdId FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER) ELSE NULL END) AS geoLocation,
 	  status as 'status'
 	  FROM AccessPoint AP
 	  INNER JOIN [BossID].[dbo].FraksjonsType FT ON ft.IDFraksjon = AP.IDFraksjon
