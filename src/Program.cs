edd398824d10832037958b024c180569bcbb20d8
WasteIQ BIR <tvedtorama@gmail.com>
2019-01-28 09:19:02 +0100

    Support for replacing db before running queries. Set timeout to avoid craash on customer Query.

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,10 @@ using Microsoft.Extensions.Configuration;
 
 namespace JsonReader
 {
+    class DBConfig {
+        public string dbReplace {get; set;}
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -17,9 +21,12 @@ namespace JsonReader
 
             var fetcherConfig = configuration.GetSection("eventFetcher").Get<Fetchers.FetchEvents.FetchEventsConfig>();
             var connectionString = configuration.GetConnectionString("Main");
+            var dbReplace = configuration.GetSection("db").Get<DBConfig>().dbReplace;
 
             var conn = QueryExecuter.OpenConnection(connectionString);
-            System.Func<string, string> readFile = f => System.IO.File.ReadAllText(f);
+            var dbRegex = new System.Text.RegularExpressions.Regex(@"\[BossID\].\[dbo\]");
+            System.Func<string, string> readFileRaw = f => System.IO.File.ReadAllText(f);
+            System.Func<string, string> readFile = f => dbRegex.Replace(readFileRaw(f), dbReplace);
             var queries = new List<IEnumerable<Job>>{
                     Fetchers.FetchIdentities.ProduceIdentities(readFile),
                     Fetchers.FetchCustomers.ProduceCustomers(readFile),
@@ -36,7 +43,7 @@ namespace JsonReader
 					WithExecutionMode(ParallelExecutionMode.ForceParallelism).
 					WithDegreeOfParallelism(2).Select(x => {
 						System.Console.WriteLine("Executing: " + x.FilePath);
-                        return new {Job = x, Result = QueryExecuter.Execute((cmd) => new System.Data.SqlClient.SqlCommand(cmd, conn), x.Query, x.Params)};
+                        return new {Job = x, Result = QueryExecuter.Execute((cmd) => new System.Data.SqlClient.SqlCommand(cmd, conn) {CommandTimeout = 180}, x.Query, x.Params)};
                     })) {
 				System.Console.WriteLine("Done with this: " + x.Job.FilePath + "  " + x.Result.Substring(0, System.Math.Min(x.Result.Length, 60)));
 				System.IO.File.WriteAllText(System.IO.Path.Combine("data", x.Job.FilePath), x.Result);
