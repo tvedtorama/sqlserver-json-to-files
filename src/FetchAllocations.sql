3532a1fa0a95d2cb6761509f7813f9c9a91cb916
Jørgen Tvedt <tvedtorama@gmail.com>
2019-03-08 12:34:58 +0100

    Moved keys to props and external keys

--- src/FetchAllocations.sql
+++ src/FetchAllocations.sql
@@ -1,10 +1,13 @@
+-- geoLocationCode should perhaps be postfixed with (HA,BB) for customer types - since this cannot be properties if codes are to be unique
+
 /* Fetch allocation data, linking addresses to point groups */
-SELECT CAST(MAX(TMG.IDMatrikkelGate) as nvarchar(200)) allocationTemplateId, MAX(T.GUIDTjeneste) serviceReference, max(T.Navn) serviceName,
+SELECT CAST(MAX(TMG.IDMatrikkelGate) as nvarchar(200)) allocationTemplateId,
 	(SELECT CAST(OtherTMG.IDPunkt as nvarchar(200)) "id", (OtherTMG.IDPunktRolle + 1) "priority" FROM [BossID].[dbo].TilordningMatrikkelGate OtherTMG WHERE
 			TMG.IDMatrikkelGate = OtherTMG.IDMatrikkelGate ORDER BY IDPunktRolle FOR JSON PATH) points,
-	MAX(TMG.TilordneHusholdning) as assignHousehold, MAX(TMG.TilordneBedrift) as assignBusinesss,
-	MAX(MG.AdresseGate) as geoLocationCode,
-	MAX(MG.Gateadresse) as geoLocationName,
+	MAX(TMG.TilordneHusholdning) as "properties.assignHousehold",
+	MAX(TMG.TilordneBedrift) as "properties.assignBusiness",
+	MAX(MG.AdresseGate) as "externalKeys.geoLocationCode",
+	MAX(MG.Gateadresse) as "properties.geoLocationName",
 	MAX(TMG.IDMatrikkelGate) as "externalKeys.bossId"
 	FROM [BossID].[dbo].TilordningMatrikkelGate TMG
 	INNER JOIN [BossID].[dbo].MatrikkelGate MG ON TMG.IDMatrikkelGate = MG.IDMatrikkelGate
