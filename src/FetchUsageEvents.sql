2f6aa6fdc386ebdda2192a3c5c9e5049bd3a76a0
Jørgen Tvedt <tvedtorama@gmail.com>
2019-04-05 11:18:25 +0200

    Updated event import to support dataWindowStart/end

--- src/FetchUsageEvents.sql
+++ src/FetchUsageEvents.sql
@@ -1,9 +1,15 @@
+-- declare @startDate DateTime = '2019-04-03 00:00:00.000'
+-- declare @endDate DateTime = '2019-04-04 00:00:00.000'
+-- declare @IDTjeneste int = 8
+
 SELECT
 		'IMPORT_EVENT_BLOCK' as type,
 		'EVENT' as scenarioId,
 		JSON_QUERY((SELECT
 			CONVERT(date, k0.HendelseTidspunkt) as "theDay",
-			CONVERT(nvarchar, IDTjeneste) as "serviceClass",
+			CONCAT((SELECT Navn FROM [BossID].[dbo].BossIDTjeneste B WHERE B.IdTjeneste = K0.idtjeneste) , '_', CONVERT(nvarchar, IDTjeneste)) as "serviceClass",
+			CONVERT(bigint, DATEDIFF(second,{d '1970-01-01'},@startDate)) * 1000 as dataWindowStart,
+			CONVERT(bigint, DATEDIFF(second,{d '1970-01-01'},@endDate)) * 1000 as dataWindowEnd,
 			JSON_QUERY((SELECT HendelseTidspunkt as timestamp,
 				CASE K.IDHendelseType WHEN 1 THEN 'ACCEPTED' ELSE 'REJECTED' END as result,
 				'USE' as type,
@@ -20,4 +26,8 @@ SELECT
 				-- Note: When the IDTjeneste condition was added, to fix duplicate events among services, the query slowed down considerably
 				WHERE CONVERT(date, K.HendelseTidspunkt) = CONVERT(date, K0.HendelseTidspunkt) AND K.IDTjeneste = k0.IDTjeneste FOR JSON PATH)) AS "eventList"
 			FOR JSON PATH, WITHOUT_ARRAY_WRAPPER)) AS "payload"
-	FROM [BossID].[dbo].KundeHendelser AS K0 WHERE K0.HendelseTidspunkt >= @startDate AND K0.HendelseTidspunkt < @endDate GROUP BY CONVERT(date, K0.HendelseTidspunkt), IDTjeneste FOR JSON Path
+	FROM [BossID].[dbo].KundeHendelser AS K0
+	WHERE K0.HendelseTidspunkt >= @startDate AND K0.HendelseTidspunkt < @endDate 
+	--    AND IDTjeneste = @IDTjeneste
+	GROUP BY CONVERT(date, K0.HendelseTidspunkt), IDTjeneste
+	FOR JSON Path
